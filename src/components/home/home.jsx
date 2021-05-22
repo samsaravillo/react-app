7dab7c9200d23644a1e48069cb99afabb60052af
samiwokx <samsaravillo@gmail.com>
2021-05-22 18:04:24 +0800

    sample json data added for testing

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -3,6 +3,7 @@ import { Alert } from "reactstrap";
 import Slider from "./../slider";
 import Chart from "./../chart";
 import Contacts from "./../contacts";
+import CreateOption from "../common/createOption/createOption";
 
 const Home = () => {
   const [$contacts, setContacts] = useState([]);
@@ -25,6 +26,9 @@ const Home = () => {
   return (
     <React.Fragment>
       <Alert color="primary">This is a primary alert — check it out!</Alert>
+      <div>
+        <CreateOption />
+      </div>
       <div>
         <Slider></Slider>
         <Chart></Chart>
--- src/components/services/mockData.js
+++ src/components/services/mockData.js
@@ -24,3 +24,69 @@ export const modelsData = [
     reliability: 90,
   },
 ];
+
+
+export const option = [
+  {
+    "optionId": 1, 
+    "optionName": "Option 1",
+    "optionStatus": "RENEW",
+    "line": "ABC",
+    "system": "SYS",
+    "username": "sam",
+    "intvData": [{
+      "id": 1,
+      "scope": "SYS",
+      "subSystem": "SUBSYS",
+      "type": "TYPE01",
+      "subType": "SUBTYPE01",
+      "ÿear": 2021,
+      "status": "DRAFT",
+      "list": [{
+        "year": "2021",
+        "amount": 500,
+      },
+      {
+        "year": "2022",
+        "amount": 600,
+      }]
+    }, {
+      "id": 2,
+      "scope": "SYS",
+      "subSystem": "SUBSYS2",
+      "type": "TYPE02",
+      "subType": "SUBTYPE02",
+      "ÿear": 2023,
+      "status": "DRAFT",
+      "list": [{
+        "year": "2021",
+        "amount": 800,
+      },
+      {
+        "year": "2022",
+        "amount": 300,
+      }]
+    }, {
+      "id": 3,
+      "scope": "SYS",
+      "subSystem": "SUBSYS3",
+      "type": "TYPE03",
+      "subType": "SUBTYPE03",
+      "ÿear": 2025,
+      "status": "DRAFT",
+      "list": [{
+        "year": "2021",
+        "amount": 200,
+      },
+      {
+        "year": "2022",
+        "amount": 100,
+      }]
+    }]
+  }
+]
+
+
+
+
+
